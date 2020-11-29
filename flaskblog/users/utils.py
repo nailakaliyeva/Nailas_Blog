21b16d0f74bdfc4c539051498af90771822577fb
Naila <nailakaliyeva@gmail.com>
2020-11-29 00:11:58 -0500

    starting all over

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,5 +1,4 @@
 import os
-import requests
 import secrets
 from PIL import Image
 from flask import url_for, current_app
@@ -20,11 +19,14 @@ def save_picture(form_picture):
 
     return picture_fn
 
+
 def send_reset_email(user):
     token = user.get_reset_token()
-    message = f'''To reset your password, visit the following link:
-    {url_for('users.reset_token', token=token, _external=True)}
-    If you did not make this request, then simply ignore this email and no changes will be made.
-    '''
-    return requests.post("https://api.mailgun.net/v3/nailasblog.com/messages",auth=("api", os.getenv("API_KEY")),data={"from": "<mailgun@nailasblog.com>","to": [user.email, "nailasblog.com"],"subject": "Request to reset password","text": message})
-   
+    msg = Message('Password Reset Request',
+                  sender='nailakaliyeva@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('users.reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
